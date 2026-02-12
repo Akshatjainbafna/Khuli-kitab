1a77f792bc1cfebd75c281efd2b6e17242e7dcee
akshatjainbafna <akshatbjain.aj@gmail.com>
2026-02-12 12:18:51 +0530

    feat: Introduce Dockerfiles and Docker Compose for containerizing the frontend and backend applications.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   devIndicators: false,
+  output: 'standalone',
 
   // This silences the warning and makes Turbopack use tsconfig paths.
   turbopack: {}
